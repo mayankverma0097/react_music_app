6c473780639004a23740ebeb95683ec1de980bef
Mayank Verma <mayankverma0097@hotmail.com>
2025-03-28 18:19:08 +0530

    changed to light theme till Artist

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
-import "./SideBar.css"
-import { House,Search,BookOpen } from 'lucide-react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./SideBar.css";
+import { House, Search, BookOpen, Menu } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function SideBar() {
+  let [isOpen, setisOpen] = useState(false);
+
+  function toggleSidebar() {
+    setisOpen(!isOpen);
+    console.log("toggle button clicked");
+  }
   return (
     <div>
-     <div id="SideBar_Main">
-      <p id='SideBar_Main_siteName'><Link to={"/"}>WaveTune</Link></p>
-      
-        <ul>
-          <li><Link to={"/Home"}> <House size={20}  />  <span className='SideBar_P'>Home</span></Link></li>
-            
-            <li> <Link to={"/Home"}> <Search size={20}  />  <span className='SideBar_P'>Search</span></Link></li>
+      <div id="SideBar_Main" className={`${isOpen ? "Open" : ""}`}>
+        <p id="SideBar_Main_siteName">
+          <Link to={"/"}>WaveTune</Link>
+        </p>
 
-            <li><Link to={"/Home"}> <BookOpen size={20}  />  <span className='SideBar_P'>Library</span></Link></li>
+        <ul>
+          <li>
+            <Link to={"/Home"}>
+              {" "}
+              <House size={20} /> <span className="SideBar_P">Home</span>
+            </Link>
+          </li>
+
+          <li>
+            {" "}
+            <Link to={"/Home"}>
+              {" "}
+              <Search size={20} /> <span className="SideBar_P">Search</span>
+            </Link>
+          </li>
+
+          <li>
+            <Link to={"/Home"}>
+              {" "}
+              <BookOpen size={20} /> <span className="SideBar_P">Library</span>
+            </Link>
+          </li>
         </ul>
-     </div>
+
+        <div id="SideBar_Toggle_button" onClick={toggleSidebar}>
+          <Menu />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
